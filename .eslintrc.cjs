6f84269616e5b4d8455896714697db98b798e56d
Maratbek Erken <maratbekerken@gmail.com>
2026-07-08 20:53:27 +0500

    init: start async-race task

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2023: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.app.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
